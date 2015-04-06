cc1e3618b4d6a7e550ba9e6dcb4a5c5e41091f6d
Kim Nilsson <kn222gp@student.lnu.se>
2015-04-06 19:10:30 +0200

    Made background, player movement and laser.

---
+++ 2Dtest/2D Testproject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMax, xMin, yMax, yMin;
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	void Update()
+	{
+		if(Input.GetButton("Fire1") && Time.time > nextFire)
+	    {
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+
+	}
+
+	// Physics
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+		GetComponent<Rigidbody> ().position = new Vector2
+			(
+				Mathf.Clamp(GetComponent<Rigidbody> ().position.x, boundary.xMin, boundary.xMax),
+				Mathf.Clamp(GetComponent<Rigidbody>().position.y, boundary.yMin, boundary.yMax)
+			);
+	}
+
+
+}
