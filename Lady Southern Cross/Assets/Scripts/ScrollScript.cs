89bd91ed2d37c4c29aed2e48922a5713101ec0a6
Kim Nilsson <kn222gp@student.lnu.se>
2015-04-13 23:35:44 +0200

    Fixed camera bug in web player

---
+++ Lady Southern Cross/Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollScript : MonoBehaviour 
+{
+	public float speed;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (0.0f, Time.time * speed);
+	}
+}
